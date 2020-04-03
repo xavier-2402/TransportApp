f4585516cbccd405d1997c92d529bf904299465c
Jessica Alvarez <jesicaalvares2@hotmail.com>
2020-04-03 14:43:02 -0500

    MApa Metodo

--- app/src/main/java/com/example/transportapp/Mapa.java
+++ app/src/main/java/com/example/transportapp/Mapa.java
@@ -23,6 +23,7 @@ public class Mapa extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
 
