b2e95f1794249ff89d330ff4aad07a5e67e1c25c
Franklin <franklinlucero62@gmail.com>
2020-04-15 20:37:34 -0500

    menu q no se q mismo :v

--- app/src/main/java/com/example/transportapp/ui/main/Transportista.java
+++ app/src/main/java/com/example/transportapp/ui/main/Transportista.java
@@ -1,5 +1,6 @@
 package com.example.transportapp.ui.main;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -12,10 +13,14 @@ import android.widget.EditText;
 
 import com.example.transportapp.R;
 
+import java.io.File;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Transportista extends Fragment {
+
+
 
     public Transportista() {
         // Required empty public constructor
@@ -69,5 +74,7 @@ public class Transportista extends Fragment {
 
         return retorno;
     }
+
+
 
 }
