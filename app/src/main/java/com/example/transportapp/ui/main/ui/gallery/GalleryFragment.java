3081b83a6ba40a6f596aecfb28b2d3a569671f63
Franklin <franklinlucero62@gmail.com>
2020-04-16 13:36:40 -0500

    arreglo de error

--- app/src/main/java/com/example/transportapp/ui/main/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/transportapp/ui/main/ui/gallery/GalleryFragment.java
@@ -12,7 +12,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
-import com.example.transportapp.ui.main.R;
+import com.example.transportapp.R;
 
 public class GalleryFragment extends Fragment {
 
