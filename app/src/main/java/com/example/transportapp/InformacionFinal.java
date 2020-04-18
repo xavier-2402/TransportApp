ab1906af7dbe0502d09d55134bdf6272d94e7f1e
Xavier Yanza <xavier.y.2013@gmail.com>
2020-04-18 05:46:51 -0500

    correo

--- app/src/main/java/com/example/transportapp/InformacionFinal.java
+++ app/src/main/java/com/example/transportapp/InformacionFinal.java
@@ -6,22 +6,45 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class InformacionFinal extends AppCompatActivity {
 
     private Button finalizar;
+    Session session = null;
+    ProgressDialog pdialog = null;
+    Context context = null;
+    String  asunto, mensaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informacion_final);
+        context = this;
 
         finalizar = (Button)findViewById(R.id.btnFinalizar);
         finalizar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Abrir();
+                enviarCorreo();
+              //Abrir();
             }
         });
     }
@@ -30,8 +53,66 @@ public class InformacionFinal extends AppCompatActivity {
 
     public void Abrir(){
 
-        Intent intent  =new Intent(getApplicationContext(),InformacionFinal.class);
+        Intent intent  =new Intent(getApplicationContext(),SolicitudServicio.class);
+
         startActivity(intent);
         finish();
     }
+
+    public void enviarCorreo(){
+        asunto = "Solicitud de servicio";
+        mensaje = "Su vehiculo ha sido reservado para un servicio de mudanza \n" +
+                "Cliente: Jessica Alvarez \n" +
+                "Fecha: 18/04/2020 \n" +
+                "Hora: 08:00-18:00 \n" +
+                " Direccion: Cuenca";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+
+        session = Session.getDefaultInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("xyanza2000@gmail.com", "xavier-2402@");
+            }
+        });
+
+        pdialog = ProgressDialog.show(context, "", "Sending Mail...", true);
+
+        RetreiveFeedTask task = new RetreiveFeedTask();
+        task.execute();
+
+    }
+
+    class RetreiveFeedTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try{
+                Message message = new MimeMessage(session);
+                message.setFrom(new InternetAddress("testfrom354@gmail.com"));
+                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("xavier.y.2013@gmail.com"));
+                message.setSubject(asunto);
+                message.setContent(mensaje, "text/html; charset=utf-8");
+                Transport.send(message);
+            } catch(MessagingException e) {
+                e.printStackTrace();
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            pdialog.dismiss();
+            Toast.makeText(getApplicationContext(), "Message sent", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/transportapp/SolicitudServicio.java
+++ app/src/main/java/com/example/transportapp/SolicitudServicio.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+
 public class SolicitudServicio extends AppCompatActivity implements MapasFragment.OnFragmentInteractionListener {
 
     private Button btnTrazar;
